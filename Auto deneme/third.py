81c051fa5afb25d78cd6848de5b7adec1eaea13e
af-kuas <af.kuas@gmail.com>
2020-09-05 19:58:35 +0300

    update

---
+++ Auto deneme/third.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+chrome_browser = webdriver.Chrome("./chromedriver")
+chrome_browser.maximize_window()
+chrome_browser.get(
+    "https://sabis.sakarya.edu.tr/tr/Login?return=http%3a%2f%2fsabis.sakarya.edu.tr%2f")
+# sleep(10)
+
+chrome_browser.find_element_by_id(
+    "UserName").send_keys("g131210055")
+chrome_browser.find_element_by_id(
+    "Password").send_keys("haggard04")
+chrome_browser.find_element_by_id("btnLogin").click()
+chrome_browser.find_element_by_link_text("ÖĞRENCİ BİLGİ SİSTEMİ").click()
+chrome_browser.find_element_by_id("Username").send_keys("g131210055")
+chrome_browser.find_element_by_id("Password").send_keys("haggard04")
+chrome_browser.find_element_by_name(
+    "button").click()
+chrome_browser.find_element_by_partial_link_text("Diğer İşlemler").click()
+chrome_browser.find_element_by_partial_link_text("Seçilen Dersler").click()
+
+sleep(8)
+chrome_browser.find_element_by_partial_link_text("Transkript").click()
+
+body = chrome_browser.find_element_by_css_selector('body')
+body.send_keys(Keys.END)
